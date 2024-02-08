a9b55f677db25ac5f4cda1856c7e39e07bcb6823
fbartusch <felix.bartusch@uni-tuebingen.de>
2024-02-08 05:51:21 +0100

    Fix bugs when collecting input/output data
    Fixed bug that causes multiple entries for the same file.
    Fixed bug that tries to add directories. Directories are now traversed recursively.

--- plugins/nf-provone/src/main/nextflow/provone/ProvoneObserver.groovy
+++ plugins/nf-provone/src/main/nextflow/provone/ProvoneObserver.groovy
@@ -79,6 +79,11 @@ class ProvOneObserver implements TraceObserver {
     Execution workflowExecution = null
     User user = null
 
+    //TODO In the test example, reflengths.bin two times present in the output
+    // Need to keep track for which file we already added as Data element to the document.
+    // As files can be output of a process and input to other processes, this would result in multiple similar
+    // Data elements in the document
+
     /**
      * The is method is invoked when the flow is going to start
      */
@@ -333,7 +338,56 @@ class ProvOneObserver implements TraceObserver {
         WasPartOf progPartOfWF = pFactory.newWasPartOf(progQN, workflow.getId())
         document.getStatementOrBundle().add(progPartOfWF)
 
+        //TODO there seems to be a directory present in the output?
+        // -> Rework the data generation ...
+
+        // Compile a list of all input files
+        // TODO If possible, move this to onProcessStart or somewhere else where we can see the input files
+        //  Otherwise output files could be listed as input files, if there are output files in input directories after
+        //  the process finished
+        List<Path> inputFilesList = new LinkedList<Path>();
+        Map<FileInParam, Object> fileInParams = handler.getTask().getInputsByType(FileInParam.class)
+        for (entry in fileInParams) {
+
+            File fileInParam = null
+
+            // entry.value could be an ArrayBag containing a FileHolder object
+            if (entry.value.getClass() == ArrayBag.class) {
+                ArrayBag arrayBag = (ArrayBag) entry.value
+
+                // Value stored in the array bag could be a FileHolder object
+                if (arrayBag.get(0).class == FileHolder.class) {
+                    FileHolder fileHolder = (FileHolder) arrayBag.get(0)
+                    fileInParam = new File(fileHolder.getSourceObj().toString())
+                }
+            }
+
+            if (entry.value.getClass() == FileInParam.class) {
+                fileInParam = new File(entry.value.toString())
+            }
+
+            // If the parameter is a directory, we have to collect files.
+            if (fileInParam.isDirectory()) {
+                inputFilesList.addAll(listOfFiles(fileInParam.toPath()))
+            }
+            if (fileInParam.isFile()) {
+                inputFilesList.add(fileInParam.toPath())
+            }
+        }
+
+        // Create Data elements for each output file
+        for (Path inputFile in inputFilesList) {
+            Data data = createData(inputFile.toFile())
+            //document.getStatementOrBundle().add(data)
+            addDataToDocument(data)
+
+            // Current process (ProvONE: execution) used that file
+            document.getStatementOrBundle().add(pFactory.newUsed(processExe.id, data.id))
+        }
+
+
         // Input file(s) of the Execution
+        /*
         Map<FileInParam, Object> inputFiles = handler.getTask().getInputsByType(FileInParam.class)
         for (entry in inputFiles) {
             File inputPath = null
@@ -356,7 +410,8 @@ class ProvOneObserver implements TraceObserver {
             // Add the file to the document
             if (inputPath.isFile()) {
                 Data data = createData(inputPath)
-                document.getStatementOrBundle().add(data)
+                //document.getStatementOrBundle().add(data)
+                addDataToDocument(data)
 
                 // Current process (ProvONE: execution) used that file
                 document.getStatementOrBundle().add(pFactory.newUsed(processExe.id, data.id))
@@ -367,48 +422,69 @@ class ProvOneObserver implements TraceObserver {
                 // Iterate over the directory content
                 File[] dirListing = inputPath.listFiles()
                 if (dirListing != null) {
+                    //TODO check if directory is added here ...
+
                     for (File child : dirListing) {
                         Data data = createData(child)
-                        document.getStatementOrBundle().add(data)
+                        //document.getStatementOrBundle().add(data)
+                        addDataToDocument(data)
 
                         // Current process (ProvONE: execution) used that file
                         document.getStatementOrBundle().add(pFactory.newUsed(processExe.id, data.id))
                     }
                 }
             }
+        }*/
+
+        // Compile a list of all output files
+        List<Path> outputFilesList = new LinkedList<Path>();
+        Map<FileOutParam, Object> fileOutParams = handler.getTask().getOutputsByType(FileOutParam.class)
+        for (entry in fileOutParams) {
+            File fileOutParam = new File(entry.value.toString())
+            // If the parameter is a directory, we have to collect files.
+            if (fileOutParam.isDirectory()) {
+               outputFilesList.addAll(listOfFiles(fileOutParam.toPath()))
+            }
+            if (fileOutParam.isFile()) {
+                outputFilesList.add(fileOutParam.toPath())
+            }
         }
 
-        //TODO In the test example, reflengths.bin two times present in the output
-        // Output file(s) of the Execution
-        Map<FileOutParam, Object> outputFiles = handler.getTask().getOutputsByType(FileOutParam.class)
-        for (entry in outputFiles) {
-            File outputFile = new File(entry.value.toString())
+        // Create Data elements for each output file
+        for (Path outputFile in outputFilesList) {
+            Data data = createData(outputFile.toFile())
+            //document.getStatementOrBundle().add(data)
+            addDataToDocument(data)
 
-            if (outputFile.isDirectory()) {
-                // Iterate over the directory content
-                File[] dirListing = outputFile.listFiles()
-                if (dirListing != null) {
-                    for (File child : dirListing) {
-                        Data data = createData(child)
-                        document.getStatementOrBundle().add(data)
-
-                        // Current process (ProvONE: execution) generated that file
-                        document.getStatementOrBundle().add(pFactory.newWasGeneratedBy(data, null, processExe))
-                    }
-                }
-            }
-
-            if (outputFile.isFile()) {
-                Data data = createData(outputFile)
-                document.getStatementOrBundle().add(data)
-                // Current process (ProvONE: execution) generated that file
-                document.getStatementOrBundle().add(pFactory.newWasGeneratedBy(data, null, processExe))
-            }
+            // Current process (ProvONE: execution) generated that file
+            document.getStatementOrBundle().add(pFactory.newWasGeneratedBy(data, null, processExe))
         }
 
         // TODO wasInformedBy
         // prov:wasInformedBy is adopted in ProvONE to state that an Execution communicates with another
         // Execution through an output-input relation, and thereby triggers its execution.
+    }
+
+    private void addDataToDocument(Data data) {
+        if ( ! document.getStatementOrBundle().contains(data)) {
+            document.getStatementOrBundle().add(data)
+        }
+    }
+
+
+    private static List<Path> listOfFiles(Path dirPath) {
+        List<Path> result = new ArrayList<Path>();
+
+        Path[] filesList = dirPath.listFiles();
+        for(Path file : filesList) {
+            if(file.isFile()) {
+                result.add(file)
+            } else {
+                result.addAll(listOfFiles(file))
+            }
+        }
+
+        return result
     }
 
     /**
@@ -512,8 +588,10 @@ class ProvOneObserver implements TraceObserver {
 
     private Data createData(File file)  {
         // Compute hash sum of the file
-        String checksum = getSha1sum(file)
-        QualifiedName outputQN = pFactory.newQualifiedName("https://example.com/", checksum, "ex");
-        return pFactory.newData(outputQN, file.getName())
+        //String checksum = getSha1sum(file)
+        //QualifiedName outputQN = pFactory.newQualifiedName("https://example.com/", checksum, "ex");
+        return pFactory.newData(Path.of(file.toString()), "https://example.com/", "ex")
     }
+
+
 }
